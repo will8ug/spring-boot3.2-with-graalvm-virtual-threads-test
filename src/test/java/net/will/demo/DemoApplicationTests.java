12b19bf87eb9ab964a027e0659a275453e957341
Will <yzj_000@163.com>
2023-12-29 19:00:15 +0800

    initial version

---
+++ src/test/java/net/will/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.will.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
